effd34724aa9417873e8c833b2ba7f41c6523348
Maksym Volkov <maksymvo@tahometer.com>
2019-03-22 18:05:54 +0200

    Getting tasks by project name

--- src/main/java/com/example/server/controller/ProjectController.java
+++ src/main/java/com/example/server/controller/ProjectController.java
@@ -1,11 +1,15 @@
 package com.example.server.controller;
 
+import com.example.server.dto.TaskDto;
 import com.example.server.repo.Project;
 import com.example.server.repo.ProjectRepository;
+import com.example.server.repo.Task;
+import com.example.server.repo.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/projects")
@@ -14,6 +18,8 @@ public class ProjectController {
 
     @Autowired
     private ProjectRepository projectRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
     @GetMapping
     public List<Project> getProjects() {
@@ -23,5 +29,13 @@ public class ProjectController {
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public Project getProjectByName(@PathVariable("name") String name) {
         return projectRepository.findByName(name);
+    }
+
+    @RequestMapping(value = "/{name}/tasks", method = RequestMethod.GET)
+    public List<TaskDto> getTasksByProjectName(@PathVariable("name") String name) {
+        Project project = projectRepository.findByName(name);
+        return taskRepository.findAllByProjectId(project.getId()).stream()
+                .map(TaskDto::toDto)
+                .collect(Collectors.toList());
     }
 }
